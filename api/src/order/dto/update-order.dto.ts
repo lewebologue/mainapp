a4f9f0e4ee44bdecd4ae7648d28b74126f3d1743
Ludovic Thuard <thuard.ludovic@gmail.com>
2025-03-19 18:58:02 +0100

    refactor: simplification de UpdateOrderDto en supprimant l'héritage et en rendant les propriétés optionnelles

--- api/src/order/dto/update-order.dto.ts
+++ api/src/order/dto/update-order.dto.ts
@@ -1,14 +1,7 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateOrderDto } from './create-order.dto';
-import { Cake } from '@prisma/client';
-
-export class UpdateOrderDto extends PartialType(CreateOrderDto) {
-  id: string;
-  customerId: string;
-  cakes: Cake[];
-  total: number;
-  Withdrawal_date: Date;
-  Delivered: boolean;
-  createdAt: Date;
-  updatedAt: Date;
+export class UpdateOrderDto {
+  customer?: { connect: { id: string } };
+  cakes?: { connect: { id: string }[] };
+  total?: number;
+  Withdrawal_date?: Date;
+  delivered?: boolean;
 }
