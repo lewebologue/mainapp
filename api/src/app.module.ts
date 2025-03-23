209838944ebd44dcd17bb988bcd4f5a5bc4394ee
Ludovic Thuard <thuard.ludovic@gmail.com>
2025-03-23 10:04:43 +0100

    refactor: suppression des fichiers AppController et AppService, ajout de PrismaService dans les modules User et Order

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CakeModule } from './cake/cake.module';
 import { OrderModule } from './order/order.module';
@@ -8,7 +6,5 @@ import { KpiModule } from './kpi/kpi.module';
 
 @Module({
   imports: [UserModule, CakeModule, OrderModule, KpiModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,42 +1,55 @@
+import { Controller, Get, Post, Body, Patch, Param } from '@nestjs/common';
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBody,
+  ApiParam,
+} from '@nestjs/swagger'; // Import Swagger decorators
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+@ApiTags('user') // Ajout d'un tag pour regrouper les routes dans Swagger
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Créer un nouvel utilisateur' }) // Description de l'opération
+  @ApiBody({ type: CreateUserDto }) // Documentation du corps de la requête
+  @ApiResponse({ status: 201, description: 'Utilisateur créé avec succès.' }) // Réponse en cas de succès
+  @ApiResponse({ status: 400, description: 'Requête invalide.' }) // Réponse en cas d'erreur
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
-
   @Get(':id')
+  @ApiOperation({ summary: 'Récupérer un utilisateur par ID' }) // Description de l'opération
+  @ApiParam({ name: 'id', description: "ID de l'utilisateur", type: String }) // Documentation du paramètre
+  @ApiResponse({
+    status: 200,
+    description: 'Utilisateur récupéré avec succès.',
+  }) // Réponse en cas de succès
+  @ApiResponse({ status: 404, description: 'Utilisateur non trouvé.' }) // Réponse en cas d'erreur
   findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+    return this.userService.find({ id });
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @ApiOperation({ summary: 'Mettre à jour un utilisateur par ID' }) // Description de l'opération
+  @ApiParam({ name: 'id', description: "ID de l'utilisateur", type: String }) // Documentation du paramètre
+  @ApiBody({ type: UpdateUserDto }) // Documentation du corps de la requête
+  @ApiResponse({
+    status: 200,
+    description: 'Utilisateur mis à jour avec succès.',
+  }) // Réponse en cas de succès
+  @ApiResponse({ status: 400, description: 'Requête invalide.' }) // Réponse en cas d'erreur
+  @ApiResponse({ status: 404, description: 'Utilisateur non trouvé.' }) // Réponse en cas d'erreur
+  update(@Param('id') id: string, @Body() updateUser: UpdateUserDto) {
+    return this.userService.updateUser({
+      where: { id },
+      data: updateUser,
+    });
   }
 }
