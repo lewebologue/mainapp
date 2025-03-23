a4847e2db5d3038719eae55a569befe37ee077ee
Ludovic <thuard.ludovic@gmail.com>
2025-03-23 18:04:13 +0100

    Init customer crud

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,8 +3,9 @@ import { UserModule } from './user/user.module';
 import { CakeModule } from './cake/cake.module';
 import { OrderModule } from './order/order.module';
 import { KpiModule } from './kpi/kpi.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [UserModule, CakeModule, OrderModule, KpiModule],
+  imports: [UserModule, CakeModule, OrderModule, KpiModule, CustomerModule],
 })
 export class AppModule {}
