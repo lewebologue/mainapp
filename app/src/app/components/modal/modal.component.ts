81c3004b49fbe4bacf952b1f409b72675cb47207
Ludovic Thuard <thuard.ludovic@gmail.com>
2025-03-28 22:21:28 +0100

    feat: Ajout d'un formulaire dans le modal pour l'ajout de gâteaux avec validation et navigation par étapes

--- app/src/app/components/modal/modal.component.ts
+++ app/src/app/components/modal/modal.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, inject, Input} from '@angular/core';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
 
 @Component({
   selector: 'app-modal',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
+  ],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
 })
 export class ModalComponent {
+  @Input() title!: string;
+  @Input() message!: string;
+  @Input() validateButtonText!: string;
+  @Input() cancelButtonText!: string;
+  @Input() modalType!: string;
 
+  private _formBuilder = inject(FormBuilder);
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
 }
--- app/src/app/pages/cakes/cakes.component.ts
+++ app/src/app/pages/cakes/cakes.component.ts
@@ -4,15 +4,17 @@ import { ApiService } from '../../services/api.service';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from '../../components/table/table.component';
 import { MatTableDataSource } from '@angular/material/table';
+import { ModalComponent } from '../../components/modal/modal.component';
 
 @Component({
   selector: 'app-cakes',
-  imports: [CommonModule, TableComponent],
+  imports: [CommonModule, TableComponent, ModalComponent],
   templateUrl: './cakes.component.html',
   styleUrls: ['./cakes.component.css']
 })
 export class CakesComponent implements OnInit {
   apiData: MatTableDataSource<Cakes> = new MatTableDataSource<Cakes>([]);
+  addCakeModal = false;
 
   constructor(private cakesService: ApiService) {}
 
