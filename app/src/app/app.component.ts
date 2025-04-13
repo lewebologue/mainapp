10a05e568c1494409f55fad84f639368061ecb19
Ludovic Thuard <thuard.ludovic@gmail.com>
2025-04-13 13:51:45 +0200

    feat: Suppression des dépendances ngx-translate et du fichier de traduction français pour simplifier la gestion des langues

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +9,5 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  constructor(private translate: TranslateService) {
-    translate.setDefaultLang('fr');
-    translate.use('fr');
-  }
   title = 'MainApp';
-
-  switchLang(lang: string) {
-    this.translate.use(lang);
-  }
 }
--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -1,34 +1,18 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideNativeDateAdapter } from '@angular/material/core';
-import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
     provideHttpClient(),
     provideNativeDateAdapter(),
     provideToastr(),
-    importProvidersFrom(
-      TranslateModule.forRoot({
-        defaultLanguage: 'fr',
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient],
-        },
-      }),
-    ),
   ],
 };
