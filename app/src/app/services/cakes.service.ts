3e445012c069dc2b963e20ee70a8155261ab8921
Ludovic Thuard <thuard.ludovic@gmail.com>
2025-04-13 09:29:47 +0200

    feat: Mise à jour des services Clients et Gâteaux pour utiliser l'URL de l'API depuis les variables d'environnement

--- app/src/app/services/cakes.service.ts
+++ app/src/app/services/cakes.service.ts
@@ -2,27 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Cakes } from '../models/cakes.interface';
+import { environment } from '../../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class CakesService {
   constructor(private httpClient: HttpClient) {}
+  #apiBaseUrl: string = environment.apiUrl;
 
   getCakes(): Observable<Cakes[]> {
-    return this.httpClient.get<Cakes[]>('http://localhost:3000/cake');
+    return this.httpClient.get<Cakes[]>(`${this.#apiBaseUrl}/cake`);
   }
 
   createCake(cakeData: Cakes): Observable<Cakes> {
-    return this.httpClient.post<Cakes>('http://localhost:3000/cake', cakeData);
+    return this.httpClient.post<Cakes>(`${this.#apiBaseUrl}/cake`, cakeData);
   }
 
   deleteCake(id: string): Observable<Cakes> {
-    return this.httpClient.delete<Cakes>(`http://localhost:3000/cake/${id}`);
+    return this.httpClient.delete<Cakes>(`${this.#apiBaseUrl}/cake/${id}`);
   }
 
   updateCake(id: string, cakeData: Cakes): Observable<Cakes> {
     return this.httpClient.put<Cakes>(
-      `http://localhost:3000/cake/${id}`,
+      `${this.#apiBaseUrl}/cake/${id}`,
       cakeData,
     );
   }
--- app/src/app/services/clients.service.ts
+++ app/src/app/services/clients.service.ts
@@ -2,32 +2,31 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Customers } from '../models/customers.interface';
-
+import { environment } from '../../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class ClientsService {
   constructor(private httpClient: HttpClient) {}
+  #apiBaseUrl: string = environment.apiUrl;
 
   getCustomers(): Observable<Customers[]> {
-    return this.httpClient.get<Customers[]>('http://localhost:3000/customer');
+    return this.httpClient.get<Customers[]>(`${this.#apiBaseUrl}/customer`);
   }
 
   getOneCustomer(id: string): Observable<Customers> {
-    return this.httpClient.get<Customers>(
-      `http://localhost:3000/customer/${id}`,
-    );
+    return this.httpClient.get<Customers>(`${this.#apiBaseUrl}/customer/${id}`);
   }
 
   deleteCustomer(id: string): Observable<Customers> {
     return this.httpClient.delete<Customers>(
-      `http://localhost:3000/customer/${id}`,
+      `${this.#apiBaseUrl}/customer/${id}`,
     );
   }
 
   updateCake(id: string, cakeData: Customers): Observable<Customers> {
     return this.httpClient.put<Customers>(
-      `http://localhost:3000/cake/${id}`,
+      `${this.#apiBaseUrl}/cake/${id}`,
       cakeData,
     );
   }
