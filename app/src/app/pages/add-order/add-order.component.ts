4d0f258888a8ae350f126c8695124c2614248681
Ludovic Thuard <thuard.ludovic@gmail.com>
2025-04-17 13:29:34 +0200

    feat: Start order page and stepper

--- app/src/app/pages/add-order/add-order.component.ts
+++ app/src/app/pages/add-order/add-order.component.ts
@@ -1,15 +1,24 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {
   FormBuilder,
-  Validators,
+  FormControl,
+  FormGroup,
   FormsModule,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatStepperModule } from '@angular/material/stepper';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatStepperModule } from '@angular/material/stepper';
 import { ButtonComponent } from '../../components/button/button.component';
+import { Cakes } from '../../models/cakes.interface';
+import { Customers } from '../../models/customers.interface';
+import { CakesService } from '../../services/cakes.service';
+import { ClientsService } from '../../services/clients.service';
+// import { OrdersService } from '../../services/orders.service';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-add-order',
@@ -21,18 +30,52 @@ import { ButtonComponent } from '../../components/button/button.component';
     MatFormFieldModule,
     MatInputModule,
     ButtonComponent,
+    MatSelectModule,
+    MatListModule,
   ],
   templateUrl: './add-order.component.html',
   styleUrl: './add-order.component.scss',
 })
-export class AddOrderComponent {
-  private _formBuilder = inject(FormBuilder);
+export class AddOrderComponent implements OnInit {
+  constructor() {
+    this.secondFormGroup = new FormGroup({
+      cakes: this.cakeControl,
+    });
+  }
+  #formBuilder = inject(FormBuilder);
+  #clientService = inject(ClientsService);
+  #cakesService = inject(CakesService);
+  // #orderService = inject(OrdersService);
 
-  firstFormGroup = this._formBuilder.group({
+  clients!: Customers[];
+  selectedClient!: Customers;
+  cakes!: Cakes[];
+
+  firstFormGroup = this.#formBuilder.group({
     firstCtrl: ['', Validators.required],
   });
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
-  });
+  secondFormGroup!: FormGroup;
   isLinear = true;
+  cakeControl = new FormControl();
+
+  ngOnInit(): void {
+    this.getAllCakes();
+    this.getAllCustomers();
+  }
+
+  getAllCustomers() {
+    this.#clientService.getCustomers().subscribe((response: Customers[]) => {
+      this.clients = response;
+    });
+  }
+
+  getAllCakes() {
+    this.#cakesService.getCakes().subscribe((response: Cakes[]) => {
+      this.cakes = response;
+    });
+  }
+
+  createOrder() {
+    //TODO
+  }
 }
